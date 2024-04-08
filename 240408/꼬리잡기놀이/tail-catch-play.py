5e35f5969781aa08be2f0db6d3f965acf34155a8
Jihyun Lee <67469315+zzzl-523@users.noreply.github.com>
2024-04-08 14:32:44 +0900

    Title: 꼬리잡기놀이, Time: 149ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240408/꼬리잡기놀이/tail-catch-play.py
+++ 240408/꼬리잡기놀이/tail-catch-play.py
@@ -8,13 +8,16 @@ K = 0
 board = []
 starter_list = []
 score = 0
+head_reverse = False
+one_list = []
+three_list = []
 
 def solution(t):
-    global score
+    global score, head_reverse
     # 머리사람 따라서 이동
     dir = [(0,1), (-1,0), (0,-1), (1,0)]
-    for dx, dy in starter_list:
-        q = deque([(dx, dy, (0, 0), 1)])
+    for idx, xy in enumerate(starter_list):
+        q = deque([(xy[0], xy[1], (0, 0), 1)])
         while q:
             x, y, prev_dir, cnt = q.popleft()
             for dx, dy in dir:
@@ -33,17 +36,21 @@ def solution(t):
                 if new == 4:
                     board[x][y] = [2, 2]    # 맨 처음 1
                     board[nx][ny] = [1, 1]  # 2번
+                    one_list[idx] = [nx, ny]
+                    starter_list[idx] = [nx, ny]
                 elif new == 2:
                     board[x][y] = [2, cnt+1]
                     q.append((nx, ny, (dx, dy), board[x][y][1]))  # 큐에 추가
                 elif new == 3:
                     board[x][y] = [3, cnt+1]
                     board[nx][ny] = [4, 0]
+                    three_list[idx] = [x, y]
                     break
 
                 # print(*board, sep='\n')
     # 1회 이동 상태
     # print(*board, sep='\n')
+    
 
     # 2) 공 던지기
     arrow = dir[t//K]
@@ -51,15 +58,36 @@ def solution(t):
         if arrow[1] and 0<board[t%N][i][0]<4:    # 던지기 방향 왼, 오
             # 사람 마주침
             score += board[t%N][i][1]**2
+            team_num = board_idx[t%N][i]
+            
+            # 머리-꼬리 바꾸기
+            starter_list[team_num-1] = three_list[team_num-1]
+            one_xy = one_list[team_num-1]
+            three_xy = three_list[team_num-1]
+            board[one_xy[0]][one_xy[1]], board[three_xy[0]][three_xy[1]]  = board[three_xy[0]][three_xy[1]], board[one_xy[0]][one_xy[1]]
+
+            # print("changed")
+            # print("team_num: ", team_num)
+            # print("one: ", one_xy)
+            # print("three: ", three_xy)
+            # print(*board, sep='\n')
             break
         
-        if arrow[0] and 0<board[i][t%N][0]<4:    # 던지기 방향 왼, 오
+        elif arrow[0] and 0<board[i][t%N][0]<4:    # 던지기 방향 왼, 오
             score += board[i][t%N][1]**2
+            team_num = board_idx[i][t%N]
+            
+            # 머리-꼬리 바꾸기
+            starter_list[team_num-1] = three_list[team_num-1]
+            one_xy = one_list[team_num-1]
+            three_xy = three_list[team_num-1]
+            board[one_xy[0]][one_xy[1]], board[three_xy[0]][three_xy[1]]  = board[three_xy[0]][three_xy[1]], board[one_xy[0]][one_xy[1]]
+            
+            # print("changed")
+            # print(*board, sep='\n')
+
             break
-
-
-    print(score)
-                
+            
 
 if __name__ == '__main__':
     from collections import deque
@@ -67,14 +95,77 @@ if __name__ == '__main__':
     N, M, K = map(int, input().split())
     board = [list(map(int, input().split())) for _ in range(N)]
     score = 0
+    head_reverse = False
+    one_list = [(0,0) for _ in range(M)]
+    three_list = [(0,0) for _ in range(M)]
+    starter_list = [(0,0) for _ in range(M)]
 
+    # 0) 팀별 레일 표시하기
+    dir = [(0,1), (-1,0), (0,-1), (1,0)]
+    visited = [[False]*N for _ in range(N)]
+    board_idx = [[0]*N for _ in range(N)]
+    q = deque([])
+    number = 0
+    for i in range(N):
+        for j in range(N):
+            if visited[i][j] or board[i][j]==0:
+                continue
+
+            q.append((i, j))
+            visited[i][j] = True
+            number += 1
+            board_idx[i][j] = number
+
+            while q:
+                x, y = q.popleft()
+                for dx, dy in dir:
+                    nx = x + dx
+                    ny = y + dy
+
+                    if nx<0 or nx>=N or ny<0 or ny>=N:
+                        continue
+                    if board[nx][ny] == 0:
+                        continue
+                    if visited[nx][ny]:
+                        continue
+
+                    
+                    q.append((nx, ny))
+                    visited[nx][ny] = True
+                    board_idx[nx][ny] = number
+
+    # print(*board_idx, sep='\n')
     # 1) 이동
     # 전체 for문을 돌며 1의 위치 찾기
     for i in range(N):
         for j in range(N):
             board[i][j] = [board[i][j], 0]
             if board[i][j][0] == 1:
-                starter_list.append((i,j))
-
+                team_num = board_idx[i][j]
+                one_list[team_num-1] = [i,j]
+                starter_list[team_num-1] = [i,j]
+    
     for t in range(K):
         solution(t)
+    
+    print(score)
+
+
+
+
+[[4, 0], [3, 3], [2, 2], [0, 0], [0, 0], [0, 0], [0, 0]]
+[[4, 0], [0, 0], [1, 1], [0, 0], [2, 3], [2, 2], [1, 1]]
+[[4, 0], [4, 0], [4, 0], [0, 0], [3, 4], [0, 0], [4, 0]]
+[[0, 0], [0, 0], [0, 0], [0, 0], [4, 0], [0, 0], [4, 0]]
+[[0, 0], [0, 0], [4, 0], [4, 0], [4, 0], [0, 0], [4, 0]]
+[[0, 0], [0, 0], [4, 0], [0, 0], [0, 0], [0, 0], [4, 0]]
+[[0, 0], [0, 0], [4, 0], [4, 0], [4, 0], [4, 0], [4, 0]]
+[[0, 1], [1, 6]]
+[[1, 1], [2, 2], [2, 2], [0, 0], [0, 0], [0, 0], [0, 0]]
+[[4, 0], [0, 0], [1, 1], [0, 0], [3, 4], [2, 3], [2, 2]]
+[[4, 0], [4, 0], [4, 0], [0, 0], [4, 0], [0, 0], [1, 1]]
+[[0, 0], [0, 0], [0, 0], [0, 0], [4, 0], [0, 0], [4, 0]]
+[[0, 0], [0, 0], [4, 0], [4, 0], [4, 0], [0, 0], [4, 0]]
+[[0, 0], [0, 0], [4, 0], [0, 0], [0, 0], [0, 0], [4, 0]]
+[[0, 0], [0, 0], [4, 0], [4, 0], [4, 0], [4, 0], [4, 0]]
+[[0, 1], [2, 6]]
