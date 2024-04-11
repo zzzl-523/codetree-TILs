ec22d69b799687401d3f6a795494331db8fe7d97
Jihyun Lee <67469315+zzzl-523@users.noreply.github.com>
2024-04-11 18:18:15 +0900

    Title: 산타의 선물 공장, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240411/산타의 선물 공장/santa-gift-factory.py
+++ 240411/산타의 선물 공장/santa-gift-factory.py
@@ -40,13 +40,20 @@ def build_factory(contents):
     belts = {}  
 
     for m in range(1, M+1):
-        belts[m] = deque([])
+        belts[m] = {}
         for n in range((N//M)*(m-1), (N//M)*(m-1)+(N//M)):
             present = Present(ID_list[n], weight_list[n])
 
             ## 벨트에는 ID만 저장
             ## 선물은 presents 배열에 저장
-            belts[m].append(present.ID)
+            if n == (N//M)*(m-1):
+                belts[m]['head'] = present.ID
+                belts[m][present.ID] = {"prev": "head", "next": ID_list[1]}
+            if n == (N//M)*(m-1)+(N//M)-1:
+                belts[m][present.ID] = {"prev": ID_list[n-1], "next": "tail"}
+                belts[m]['tail'] = present.ID
+            else: belts[m][present.ID] = {"prev": ID_list[n-1], "next": ID_list[n+1]}
+            
             presents[present.ID] = present
     
     # print("belts: ", belts)
@@ -62,16 +69,30 @@ def unload_presents(contents):
     for key in belts.keys():
         # 맨 첫 선물 꺼내기
         belt = belts[key]
-        top = belt.popleft() # ID
+        top = belt['head'] # ID
+
+        ## 새로운 head
+        belt['head'] = belt[top]['next']
+        belt[belt['head']]['prev'] = 'head'
 
         if presents[top].weight <= w_max:
             # 선물의 무게 <= w_max 라면,
-            # 하차, queue니까 자동으로 당겨짐
+            # 하차
             # 하차된 무게 추가
             total_unload_weight += presents[top].weight
+            del belt[top]
+            
         else:
-            belt.append(top) # 아니면 다시 뒤로 넣기
-
+            # 맨 뒤에 다시 추가
+            
+            ## 기존 tail을 새로운 tail의 앞으로
+            belt[belt['tail']]['next'] = top
+            ## 새로운 tail == top
+            belt[top]['next'] = 'tail'
+            belt[top]['prev'] = belt['tail']
+            belt['tail'] = top 
+    
+        # print(belt)
     print(total_unload_weight)
 
 
@@ -79,7 +100,7 @@ def unload_presents(contents):
 def check(ID):
     for key in belts.keys():
         belt = belts[key]
-        if ID in belt:
+        if ID in belt.keys():
             return [True, key] # 벨트 번호
     return [False]
 
@@ -93,29 +114,55 @@ def remove_presents(contents):
     ck = check(r_id)
     if ck[0]: # 있으면
         b_num = ck[1]
-        belts[b_num].remove(r_id)    # 제거
-        print(r_id)                     # 출력
-    else:               # 없으면
+        now_belt = belts[b_num]
+        now = now_belt[r_id]
+
+        # 링크 갱신    
+        if now_belt['head'] == r_id:
+            now_belt['head'] = now['next']
+            now_belt[now['next']]['prev'] = 'head'
+        elif now_belt['tail'] == r_id:
+            now_belt['tail'] = now['prev']
+            now_belt[now['prev']]['next'] = 'tail'
+        else:
+            now_belt[now['prev']]['next'] = now['next']
+            now_belt[now['next']]['prev'] = now['prev']
+
+        del now_belt[r_id]     # 제거
+        # print(now_belt)
+        print(r_id) # 출력
+    else:      # 없으면
         print(-1)
 
 
 # 4. 물건 확인
 def check_presents(contents):
-    # print("[확인]")
     f_id = contents[0]
+    # print("[확인]", f_id)
     
     ck = check(f_id)
     if ck[0]:   # 있으면
         b_num = ck[1]
-        present_order = belts[b_num].index(f_id)
+        now_belt = belts[b_num]
+        now = now_belt[f_id]
+
+        # 링크 갱신
+        # now(f_id)를 head로
+        if now_belt['head'] == f_id:
+            print(b_num)
+            return
+
+        now_belt[now_belt['head']]['prev'] = f_id
+        now_belt[now['prev']]['next'] = 'tail'
+        now_belt['tail'] = now['prev']
+
+        now['prev'] = 'head'
+        now['next'] = now_belt['head']
+        now_belt['head'] = f_id
+            
         print(b_num) # 벨트 번호 출력
 
-        # print(belts[belt_idx])
-        arr = list(belts[b_num])[present_order:]
-        arr2 = list(belts[b_num])[:present_order]
-        belts[b_num] = deque(arr+arr2)
-
-        # print(belts[belt_idx])
+        # print(belts[b_num])
     else:
         print(-1)
 
@@ -137,7 +184,20 @@ def belt_error(contents):
     key_arr = list(belts.keys())
     idx = key_arr.index(b_num) # 고장난 벨트의 index -> 오른쪽 index 될 예정
     n_idx = (idx+1) % len(belts)
-    belts[key_arr[n_idx]].extend(belts[b_num])
+
+    target = belts[key_arr[n_idx]]
+    now = (belts[b_num])
+
+    target_tail = target['tail']
+    now_head = now['head']
+    now_tail = now['tail']
+
+    target[target_tail]['next'] = now_head
+    now[now_head]['prev'] = target_tail
+    target['tail'] = now_tail
+    
+    now.update(target)
+    belts[key_arr[n_idx]] = now
     
     # 고장난 벨트 제거
     del belts[b_num] 
@@ -159,7 +219,7 @@ if __name__ == '__main__':
     # 명령 실행
     for cmd, contents in commands:
         if cmd == 100: build_factory(contents)
-        # elif cmd == 200: unload_presents(contents)
+        elif cmd == 200: unload_presents(contents)
         elif cmd == 300: remove_presents(contents)
         elif cmd == 400: check_presents(contents)
         elif cmd == 500: belt_error(contents)
